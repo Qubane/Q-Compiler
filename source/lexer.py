fd6b3674941f1996470ce9d5a952d05d4d370e75
UltraQbik <no1skill@yandex.ru>
2025-01-24 12:35:37 +0300

    Add CharacterIterator class

--- source/lexer.py
+++ source/lexer.py
@@ -6,6 +6,29 @@ Lexer code
 from source.classes import *
 
 
+class CharacterIterator:
+    def __init__(self, string: str):
+        self.string: str = string
+        self._count: int = -1
+
+    def next(self) -> str | None:
+        self._count += 1
+        return self.string[self._count] if self._count < len(self.string) else None
+
+    def prev(self) -> str | None:
+        self._count -= 1
+        return self.string[self._count] if self._count > -1 else None
+
+    def is_done(self):
+        if self._count+1 >= len(self.string):
+            return True
+        return False
+
+    @property
+    def count(self) -> int:
+        return self._count
+
+
 class Lexer:
     """
     Lexer class
