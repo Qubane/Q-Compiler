50f74543a767ef9096a36a4805d14f1906e159ff
UltraQbik <no1skill@yandex.ru>
2025-01-24 12:19:44 +0300

    Add Application class

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ QM Compiler entry point file
 """
 
 
+from source.application import Application
+
+
 def main():
-    pass
+    app = Application()
+    app.run()
 
 
 if __name__ == '__main__':
--- source/application.py
+++ source/application.py
@@ -3,3 +3,15 @@ Sticks all code together
 """
 
 
+class Application:
+    """
+    Main application class
+    """
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """
+        Runs the application
+        """
