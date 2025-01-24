f06161de14ca13832385395b33887e22343793c5
UltraQbik <no1skill@yandex.ru>
2025-01-24 11:43:17 +0300

    Add Tag dataclass, Word class add __repr__ for TagType

--- source/classes.py
+++ source/classes.py
@@ -3,10 +3,34 @@ Compiler classes
 """
 
 
+from dataclasses import dataclass
 from enum import StrEnum
+from typing import Any
 
 
 class TagType(StrEnum):
     BUILT_IN = "built-in"
     POINTER = "pointer"
     NUMBER = "number"
+
+    def __repr__(self):
+        return self.value
+
+
+@dataclass
+class Tag:
+    """
+    TagType - value pair
+    """
+
+    type: TagType
+    value: Any | None = None
+
+
+class Word:
+    """
+    Instruction word
+    """
+
+    def __init__(self, tags: list[Tag]):
+        self.tags: list[Tag] = tags
