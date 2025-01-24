aca5bae7424953103a5da42a4d55a970cb9651a2
UltraQbik <no1skill@yandex.ru>
2025-01-24 12:16:48 +0300

    Add lexer class

--- source/lexer.py
+++ source/lexer.py
@@ -1,3 +1,15 @@
 """
 Lexer code
 """
+
+
+from source.classes import *
+
+
+class Lexer:
+    """
+    Lexer class
+    """
+
+    def __init__(self):
+        self.global_scope: GlobalScope = GlobalScope(list())
