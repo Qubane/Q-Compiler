cf6b5a5631ccae5c29057f53e0f81b9f5b322d07
UltraQbik <no1skill@yandex.ru>
2025-01-25 15:56:00 +0300

    Add some general definitions

--- source/built_ins.py
+++ source/built_ins.py
@@ -3,10 +3,19 @@ Build-in variables for Lexer type defining
 """
 
 
+from source.classes import *
+
+
 class General:
     """
     General built-ins, independent of CPU chosen to compile for
     """
+
+    definitions: dict[str, TagType] = {
+        "macro": TagType.BUILT_IN,
+        "subr": TagType.BUILT_IN,
+        "uses": TagType.BUILT_IN,
+    }
 
 
 class QMr11:
