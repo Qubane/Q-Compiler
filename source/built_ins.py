072010f751186cd29f43df077ae15056bd00e9a3
UltraQbik <no1skill@yandex.ru>
2025-01-25 15:53:07 +0300

    Add class namespaces for built-ins

--- source/built_ins.py
+++ source/built_ins.py
@@ -3,3 +3,19 @@ Build-in variables for Lexer type defining
 """
 
 
+class General:
+    """
+    General built-ins, independent of CPU chosen to compile for
+    """
+
+
+class QMr11:
+    """
+    Quantum Mini rev. 1.1 built-ins
+    """
+
+
+class QMr2:
+    """
+    Quantum Mini rev. 2 built-ins
+    """
