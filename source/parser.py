31ee1db040c2d7d0fdf1231c963a5d5411fbded8
UltraQbik <no1skill@yandex.ru>
2025-01-26 17:57:51 +0300

    Add first stage parsing

--- source/parser.py
+++ source/parser.py
@@ -25,7 +25,29 @@ class Parser:
     def _parse_first_stage(self):
         """
         First stage of parsing
+
+        Basic syntax checking stage
         """
+
+        for word in self.global_scope:
+            word: Word  # help type hinting
+
+            # check 'macro' and 'subr' keywords
+            if word[0].value in ["macro", "subr"] and word[1].type is not TagType.POINTER:
+                raise CompilerSyntaxError(
+                    f"built-in '{word[0].value}' followed by a non-pointer argument",
+                    line=word.line)
+
+            # check 'uses' keyword
+            for idx, tag in enumerate(word):
+                tag: Tag  # help type hinting
+                if tag.value == "uses":
+                    break
+            for tag in word[idx+1:]:
+                if tag.type is not TagType.POINTER:
+                    raise CompilerSyntaxError(
+                        f"built-in '{word[idx].value}' followed by a non-pointer argument(s)",
+                        line=word.line)
 
     def parse(self):
         """
