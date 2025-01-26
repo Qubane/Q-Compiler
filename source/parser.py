f09cdd488d10e213b36882d8d93fe956f93d6cfb
UltraQbik <no1skill@yandex.ru>
2025-01-26 19:16:26 +0300

    Add second stage parsing

--- source/parser.py
+++ source/parser.py
@@ -13,14 +13,14 @@ class Parser:
     """
 
     def __init__(self):
-        self.global_scope: GlobalScope = GlobalScope()
+        self.current_scope: Scope = Scope()
 
     def import_scope(self, scope: Scope):
         """
         Imports scope into parser
         """
 
-        self.global_scope = scope
+        self.current_scope = scope
 
     def _parse_first_stage(self):
         """
@@ -29,7 +29,7 @@ class Parser:
         Basic syntax checking stage
         """
 
-        for word in self.global_scope:
+        for word in self.current_scope:
             word: Word  # help type hinting
 
             # check 'macro' and 'subr' keywords
@@ -52,7 +52,37 @@ class Parser:
     def _parse_second_stage(self):
         """
         Second stage of parsing
+
+        Builds AST
         """
+
+        idx = -1
+        while idx < len(self.current_scope)-1:
+            idx += 1
+            word: Word = self.current_scope[idx]
+
+            # create macro and subroutine scopes
+            if word[0].value in ["macro", "subr"]:
+                # create scope
+                scope = MacroScope() if word[0].value == "macro" else SubroutineScope()
+                idx += 1
+                for word_ in self.current_scope[idx:]:
+                    word_: Word  # help type hinting
+                    if not (word_[0].type is TagType.INTERNAL and word_[0].value == ">"):
+                        break
+                    word_.pop(0)
+                    scope.add(self.current_scope.pop(idx))
+                # recursively parse scopes to make AST (Abstract Syntax Tree)
+                new_parser = Parser()
+                new_parser.import_scope(scope)
+                new_parser.parse()
+
+                # insert back into current scope
+                self.current_scope.insert(idx, scope)
+            elif word[0].type is TagType.INTERNAL and word[0].value == ">":
+                raise CompilerIndentationError(
+                    "Unexpected indent",
+                    line=word.line)
 
     def parse(self):
         """
