9c4094b14d26191f15012c59c714768483c234ad
UltraQbik <no1skill@yandex.ru>
2025-01-26 15:29:58 +0300

    Add import scope method

--- source/parser.py
+++ source/parser.py
@@ -3,7 +3,21 @@ Compiling code
 """
 
 
+from source.classes import *
+from source.built_ins import *
+
+
 class Parser:
     """
     Parser class
     """
+
+    def __init__(self):
+        self.global_scope: GlobalScope = GlobalScope()
+
+    def import_scope(self, scope: Scope):
+        """
+        Imports scope into parser
+        """
+
+        self.global_scope = scope
