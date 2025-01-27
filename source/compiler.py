981908b33d25856fd7f72ee498b75976154244f4
UltraQbik <no1skill@yandex.ru>
2025-01-27 20:46:12 +0300

    Add import scope method to compiler

--- source/compiler.py
+++ source/compiler.py
@@ -3,7 +3,21 @@ Compiling code
 """
 
 
+from source.classes import *
+from source.built_ins import *
+
+
 class Compiler:
     """
     Compiler class
     """
+
+    def __init__(self):
+        self.current_scope: Scope = Scope()
+
+    def import_scope(self, scope: Scope):
+        """
+        Imports parsed scope into compiler
+        """
+
+        self.current_scope = scope
