f332f650a6c4ea1b6e9c0bad117d44b409c1755c
UltraQbik <no1skill@yandex.ru>
2025-01-29 20:10:02 +0300

    Temp output raw TaggedInstructions

--- source/application.py
+++ source/application.py
@@ -71,5 +71,7 @@ class Application:
         compiler.compile()
 
         print("[COMPILER STAGE START]")
-        recursive_scope_print(compiler.current_scope)
+        # recursive_scope_print(compiler.current_scope)
+        for instruction in compiler.instructions:
+            print(instruction)
         print("[COMPILER STAGE END]")
