c3583fd21e730e53aa84f787719cd685d55bb9a8
UltraQbik <no1skill@yandex.ru>
2025-01-30 13:20:46 +0300

    Fix header

--- source/file_io.py
+++ source/file_io.py
@@ -4,25 +4,27 @@ File reading and writing implementations.
 
 
 from source.classes import *
+from source.built_ins import *
 
 
-def dump(data: list[InstructionN], file: str) -> int:
+def dump(data: list[InstructionN], file: str, namespace: CodeNamespace) -> int:
     """
     Dumps instruction data to a file
     :param data: instruction data
     :param file: dump filepath
+    :param namespace: used instruction namespace
     :return: amount of bytes that were written
     """
 
-    if isinstance(InstructionN, Instruction16):
-        architecture = "QM"
-    elif isinstance(InstructionN, Instruction24):
-        architecture = "QT"
+    if isinstance(namespace, NamespaceQT):
+        used_namespace = "QT"
+    elif isinstance(namespace, NamespaceQMr11):
+        used_namespace = "QM"
     else:
         raise Exception
 
     with open(file, "wb") as f:
-        f.write(f"{architecture: >8}".encode("ascii"))  # add small architecture header
+        f.write(used_namespace.encode("ascii") + b'\x00')  # add small architecture header
         for instruction in data:
             f.write(instruction.__bytes__())
-    return f.tell()
+        return f.tell()
