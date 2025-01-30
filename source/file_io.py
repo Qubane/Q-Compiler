78a7cb3282f9ace791106e24c548c40cc28ca42f
UltraQbik <no1skill@yandex.ru>
2025-01-30 13:03:13 +0300

    Maybe finish dump function

--- source/file_io.py
+++ source/file_io.py
@@ -13,3 +13,16 @@ def dump(data: list[InstructionN], file: str) -> int:
     :param file: dump filepath
     :return: amount of bytes that were written
     """
+
+    if isinstance(InstructionN, Instruction16):
+        architecture = "QM"
+    elif isinstance(InstructionN, Instruction24):
+        architecture = "QT"
+    else:
+        raise Exception
+
+    with open(file, "wb") as f:
+        f.write(f"{architecture: >8}".encode("ascii"))  # add small architecture header
+        for instruction in data:
+            f.write(instruction.__bytes__())
+    return f.tell()
