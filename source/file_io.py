7d7c3b86f83bc345987668b923ef197a6b3e72ec
UltraQbik <no1skill@yandex.ru>
2025-01-30 12:59:49 +0300

    Define dump function

--- source/file_io.py
+++ source/file_io.py
@@ -1,3 +1,15 @@
 """
 File reading and writing implementations.
 """
+
+
+from source.classes import *
+
+
+def dump(data: list[InstructionN], file: str) -> int:
+    """
+    Dumps instruction data to a file
+    :param data: instruction data
+    :param file: dump filepath
+    :return: amount of bytes that were written
+    """
