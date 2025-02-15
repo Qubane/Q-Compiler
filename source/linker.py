f98d98044a7e377be6695cbe7e4b6209dcf5adc4
UltraQbik <no1skill@yandex.ru>
2025-02-16 00:47:58 +0300

    Define code importing

--- source/linker.py
+++ source/linker.py
@@ -4,7 +4,22 @@ but will be used during preprocessor stage
 """
 
 
+from source.classes import *
+from source.built_ins import *
+from source.lexer import Lexer
+from source.parser import Parser
+
+
 class Linker:
     """
     Main linker class
     """
+
+    def __init__(self, namespace: CodeNamespace):
+        self.current_scope: Scope | None = None
+        self.code_namespace: CodeNamespace = namespace
+
+    def import_code(self, code: str):
+        """
+        Imports code into linker
+        """
